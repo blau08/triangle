7cb6cbda0540f6c9eb576927bbbd0e3231c5355b
Brian Lau & Janice Dunn <student@epicodus.com>
2015-08-10 12:22:50 -0700

    Able to return that is not a triangle.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,18 +6,24 @@ class Triangle
   end
 
   define_method(:triangle?) do
-    (@length)+(@width) >= @height ||
-    (@height)+(@width) >= @length ||
+    if (@length)+(@width) >= @height &&
+    (@height)+(@width) >= @length &&
     (@height)+(@length) >= @width
+      true
+    else
+      false
+    end
   end
 
   define_method(:type) do
-    if @length.eql?(@width) && @width.eql?(@height) && @height.eql?(@length)
-      "equilateral"
-    elsif @length.eql?(@width) || @length.eql?(@height) || @height.eql?(@width)
-      "isosceles"
-    else @length != @width && @length != @height && @width != @height
-      "scalene"
+    if triangle?()
+      if @length.eql?(@width) && @width.eql?(@height) && @height.eql?(@length)
+        "equilateral"
+      elsif @length.eql?(@width) || @length.eql?(@height) || @height.eql?(@width)
+        "isosceles"
+      else @length != @width && @length != @height && @width != @height
+        "scalene"
+      end
     end
   end
 end
